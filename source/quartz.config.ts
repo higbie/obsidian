9095795d409cfeab9e3a42017e4f2c66b8c652c9
Toby Higbie <higbie@irle.ucla.edu>
2024-12-14 09:19:10 -0800

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "Memory Work | Los Angeles",
+    pageTitleSuffix: "Documenting worker organizing in our multi-racial society.",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
