b7a0bd3036cce9cd60bde73d5fe77a157c42d54f
Toby Higbie <higbie@irle.ucla.edu>
2024-12-14 16:11:40 -0800

    change graph center force 3 -> 5

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -58,7 +58,7 @@ Component.Graph({
     depth: 1, // how many hops of notes to display
     scale: 1.1, // default view scale
     repelForce: 0.5, // how much nodes should repel each other
-    centerForce: 0.3, // how much force to use when trying to center the nodes
+    centerForce: 0.5, // how much force to use when trying to center the nodes
     linkDistance: 30, // how long should the links be by default?
     fontSize: 0.6, // what size should the node labels be?
     opacityScale: 1, // how quickly do we fade out the labels when zooming out?
@@ -71,7 +71,7 @@ Component.Graph({
     depth: -1,
     scale: 0.9,
     repelForce: 0.5,
-    centerForce: 0.3,
+    centerForce: 0.5,
     linkDistance: 30,
     fontSize: 0.6,
     opacityScale: 1,
